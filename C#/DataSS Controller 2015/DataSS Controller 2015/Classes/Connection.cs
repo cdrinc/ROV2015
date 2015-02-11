56916c8ac7cd3599ebd169d2abda0be8d0371165
Matthew Thibodeau <matt.thibodeau@carolinadesigns.com>
2015-02-10 23:32:04 -0500

    Documented Connection class
    Other classes still need HEAVY documentation

--- C#/DataSS Controller 2015/DataSS Controller 2015/Classes/Connection.cs
+++ C#/DataSS Controller 2015/DataSS Controller 2015/Classes/Connection.cs
@@ -16,20 +16,27 @@ namespace DataSS_Controller_2015.Classes
         public StreamWriter Writer;
         public NetworkStream Stream;
 
+        //"Standard Header" byte array that marks the beginning of each message
+        //is 7 bytes because the packet structure includes all values that can be above 1 (RS, RT, etc.), of which there are 6, consecutively
+        //that way it shouldn't be be able to be replicated in the packet itself
         private byte[] stx = { 0x7B, 0x7B, 0x7B, 0x7B, 0x7B, 0x7B, 0x7B };
-        public byte[] STX
+        public byte[] Header
         {
             get { return stx; }
             set { }
         }
+        //"Standard Footer" end of each message
+        //same as above
         public byte[] etx = { 0x7D, 0x7D, 0x7D, 0x7D, 0x7D, 0x7D, 0x7D };
-        public byte[] ETX
+        public byte[] Footer
         {
             get { return etx; }
             set { }
         }
 
+        //blank byte to send as handshake
         private byte[] blankData = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        //this holds the data each time ReadPacket() is called
         private byte[] packet = new byte[34];
 
         public TcpConnection(string ipAddress, int port)
@@ -62,6 +69,11 @@ namespace DataSS_Controller_2015.Classes
             Client.Close();
         }
 
+        /// <summary>
+        /// Searches the stream for a byte array (buffer), reads the buffer, and advances the stream to whatever follows.
+        /// </summary>
+        /// <param name="buffer">Byte array to search for.</param>
+        /// <returns>Returns a boolean value that indicates whether or not buffer was found in the stream.</returns>
         private bool Find(byte[] buffer)
         {
             int i = 0;
@@ -82,41 +94,68 @@ namespace DataSS_Controller_2015.Classes
             return false;
         }
 
+        /// <summary>
+        /// Exchanges a blank packet with the connected device.
+        /// </summary>
         public void Handshake()
         {
             SendPacket(blankData);
         }
 
+        /// <summary>
+        /// Determins whether data is available to be read.
+        /// </summary>
+        /// <returns>Returns a boolean value that indicates whether or not data is available.</returns>
         public bool DataAvailable()
         {
             return Stream.DataAvailable;
         }
 
+        /// <summary>
+        /// Sends a string to the connected device.
+        /// </summary>
+        /// <param name="message">String to send.</param>
         public void Send(string message)
         {
             Writer.Write(message);
             Writer.Flush();
         }
 
+        /// <summary>
+        /// Sends a single byte to the connected device.
+        /// </summary>
+        /// <param name="message">Single byte to send.</param>
         private void Send(byte message)
         {
             byte[] toSend = { message };
             Stream.Write(toSend, 0, 1);
         }
 
+        /// <summary>
+        /// Sends a byte array to the connected device.
+        /// </summary>
+        /// <param name="message">Byte array to send.</param>
         private void Send(byte[] message)
         {
             Stream.Write(message, 0, message.Length);
         }
 
+        /// <summary>
+        /// Sends a packet, consisting of a  header, data, and footer, to the connected device.
+        /// </summary>
+        /// <param name="data">Byte array of data to send. NB: at this time, packet MUST be 20 bytes.</param>
         public void SendPacket(byte[] data)
         {
-            STX.CopyTo(packet, 0);
-            data.CopyTo(packet, STX.Length);
-            ETX.CopyTo(packet, STX.Length + data.Length);
+            Header.CopyTo(packet, 0);
+            data.CopyTo(packet, Header.Length);
+            Footer.CopyTo(packet, Header.Length + data.Length);
             Send(packet);
         }
 
+        /// <summary>
+        /// Reads all data available and returns a string representation. NB: Deprecated in favor of ReadPacket()
+        /// </summary>
+        /// <returns>Returns a string representation of the bytes read from the stream.</returns>
         public string ReadAllAvailable()
         {
             List<byte> data = new List<byte>();
@@ -125,13 +164,17 @@ namespace DataSS_Controller_2015.Classes
             return System.Text.Encoding.ASCII.GetString(data.ToArray());
         }
 
+        /// <summary>
+        /// Reads a full packet, consisting of a header, data, and a footer, from the stream.
+        /// </summary>
+        /// <returns>Returns a string representation of the data suitable for display in the UI.</returns>
         public string ReadPacket()
         {
             int i = 0;
             List<byte> data = new List<byte>();
-            List<byte> footerList = ETX.ToList<byte>();
+            List<byte> footerList = Footer.ToList<byte>();
 
-            if (Find(STX))
+            if (Find(Header))
             {
                 System.Threading.Thread.Sleep(2);
                 while (Stream.DataAvailable)
@@ -162,26 +205,8 @@ namespace DataSS_Controller_2015.Classes
             {
                 parsedData = System.Text.Encoding.ASCII.GetString(data.ToArray());
             }
-            //tried to use regex
-            //string regexData = UnescapeString(parsedData);
-            //if (regexData != "")
-            //{
-            //    return regexData;
-            //}
-            return parsedData;
-        }
 
-        private string UnescapeString(string str)
-        {
-            string unescape = "";
-            string pattern = @"\d+";
-            Regex r = new Regex(pattern);
-            Match m = r.Match(str);
-            if (m.Success)
-            {
-                unescape = m.Value;
-            }
-            return unescape;
+            return parsedData;
         }
     }
 }
